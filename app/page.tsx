5581939afd078af2a1050b3ff6dfaa1afde17a9b
Daniel Franklin <daniel@danielzfranklin.org>
2024-04-16 23:51:46 +0100

    bugfix: missing suspense boundary

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import { fetchRegions } from "@/api/region";
 import GameComponent from "@/components/Game";
+import { Suspense } from "react";
 
 export default async function Home() {
   const regions = await fetchRegions();
-  return <GameComponent regions={regions} />;
+  return (
+    <Suspense>
+      <GameComponent regions={regions} />
+    </Suspense>
+  );
 }
