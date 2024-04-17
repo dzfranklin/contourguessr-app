59ef777ffdfc6d546ef527e9287fa32f2e753a97
Daniel Franklin <daniel@danielzfranklin.org>
2024-04-17 21:54:59 +0100

    Improve region logos display

--- components/RegionSelector.tsx
+++ components/RegionSelector.tsx
@@ -27,7 +27,7 @@ export default function RegionSelectorComponent({
                 <img
                   src={selectedData.logo}
                   alt=""
-                  className="h-8 rounded-sm"
+                  className="h-8 max-w-10 rounded-sm"
                 />
               ) : (
                 <span className="h-8 w-8" />
@@ -54,7 +54,7 @@ export default function RegionSelectorComponent({
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute left-0 z-10 mt-2 w-80 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute left-0 z-10 mt-2 w-80 max-h-[60vh] overflow-y-scroll origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           {regions.map((region) => (
             <Menu.Item key={region.id}>
               {({ active }) => (
@@ -62,10 +62,14 @@ export default function RegionSelectorComponent({
                   onClick={() => setSelected(region.id)}
                   className={classNames(
                     active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                    "w-full group flex items-center px-6 py-6 text-sm"
+                    "w-full group flex gap-2 items-center px-6 py-6 text-sm"
                   )}
                 >
-                  <img src={region.logo} alt="" className="h-12 mr-6" />
+                  {region.logo ? (
+                    <img src={region.logo} alt="" className="h-10 rounded-sm" />
+                  ) : (
+                    <span className="h-8 w-8" />
+                  )}
                   <span>{region.name}</span>
                 </button>
               )}
