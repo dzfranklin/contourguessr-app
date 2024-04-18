4f25ee042958b2e2f29f5b364f6623c67bdc9f15
Daniel Franklin <daniel@danielzfranklin.org>
2024-04-18 13:25:28 +0100

    Improve picture ui

--- components/Picture.tsx
+++ components/Picture.tsx
@@ -1,46 +1,100 @@
 import { Picture, PictureSize } from "@/api/picture";
 import classNames from "@/classNames";
+import { useState } from "react";
+import ArrowsPointingOutIcon from "@heroicons/react/20/solid/ArrowsPointingOutIcon";
+import ArrowsPointingInIcon from "@heroicons/react/20/solid/ArrowsPointingInIcon";
 
 export default function PictureComponent({ value }: { value?: Picture }) {
-  let pictureSize: PictureSize | undefined;
+  let regularSize: PictureSize | undefined;
+  let largeSize: PictureSize | undefined;
   for (const size of value?.sizes || []) {
-    pictureSize = size;
-    if (size.height >= 400 || size.width >= 400) break;
-  }
-  return (
-    <div className="relative h-fit w-fit p-4 mt-2 mr-2 row-start-3 col-span-full col-start-2 bg-white rounded-md shadow-md">
-      {value && pictureSize ? (
-        <img
-          src={pictureSize?.source}
-          width={pictureSize?.width}
-          height={pictureSize?.height}
-          className={classNames(
-            "rounded-sm max-w-[unset] max-h-[unset",
-            pictureSize.width > pictureSize.height ? "w-[400px]" : "h-[400px]"
-          )}
-          alt=""
-        />
-      ) : (
-        <div className="h-[400px] w-[400px] rounded-sm bg-gray-100"></div>
-      )}
+    if (size.label === "Original") {
+      // The original images has nuances like exif rotation that we don't want
+      // to deal with.
+      break;
+    }
+    if (regularSize === undefined || size.height <= 400 || size.width <= 400) {
+      regularSize = size;
+    }
 
-      {value && (
-        <div className="flex justify-between gap-4 mt-2 text-gray-600">
-          <a
-            href={value?.ownerWebpage}
-            target="_blank"
-            className="flex gap-2 items-center"
-          >
+    if (
+      largeSize === undefined ||
+      size.height > largeSize.height ||
+      size.width > largeSize.width
+    ) {
+      largeSize = size;
+    }
+  }
+
+  const [showLarge, setShowLarge] = useState(false);
+  const size = showLarge ? largeSize : regularSize;
+
+  return (
+    <div
+      className={
+        showLarge
+          ? "fixed inset-5 z-50 pointer-events-none"
+          : "w-[400px] h-[400px] mt-2 mr-2 row-start-3 col-span-full col-start-2"
+      }
+    >
+      <div
+        className={classNames(
+          "ml-auto relative pointer-events-auto max-w-full max-h-full bg-white rounded-md overflow-clip shadow-md"
+        )}
+        style={
+          size
+            ? {
+                aspectRatio: `${size.width}/${size.height}`,
+              }
+            : {
+                aspectRatio: "1/1",
+              }
+        }
+      >
+        {value && size ? (
+          <img
+            src={size.source}
+            width={size.width}
+            height={size.height}
+            alt=""
+            className={classNames("w-full h-full object-contain")}
+          />
+        ) : (
+          <div className="h-[400px] w-[400px] bg-gray-100"></div>
+        )}
+
+        <button
+          onClick={() => setShowLarge((p) => !p)}
+          className="absolute top-0 right-0 p-1.5 m-1 bg-[rgba(0,0,0,0.2)] rounded-full text-white"
+          title={showLarge ? "Smaller" : "Larger"}
+        >
+          {showLarge ? (
+            <ArrowsPointingInIcon className="w-4" />
+          ) : (
+            <ArrowsPointingOutIcon className="w-4" />
+          )}
+        </button>
+
+        {value && (
+          <div className="absolute bottom-0 left-0 right-0 p-2 grid grid-cols-[auto_minmax(0,1fr)_auto] grid-rows-1 gap-2 items-center bg-[rgba(255,255,255,0.6)] text-gray-600 text-sm">
             <img src={value?.ownerIcon} alt="" className="h-6 w-6 rounded-sm" />
-            <span className="text-sm link">{value?.ownerUsername}</span>
-          </a>
-          <div className="flex gap-2 text-sm">
-            <a className="link" href={value?.url} target="_blank">
-              View original image
+            <a
+              href={value?.ownerWebpage}
+              target="_blank"
+              className="shrink text-blue-900 underline mr-6 truncate"
+            >
+              {value?.ownerUsername}
+            </a>
+            <a
+              className="text-blue-900 underline ml-auto"
+              href={value?.url}
+              target="_blank"
+            >
+              Original image
             </a>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 }
